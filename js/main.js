0958a287e75bdf68bb971907537a869624aca5db
reneechoi02 <reneechoi02@gmail.com>
2017-04-13 21:58:03 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by renee on 2017/4/10.
+ */
+// if(window.localStorage){
+//     alert('This browser supports localStorage');
+// }else{
+//     alert('This browser does NOT support localStorage');
+// }
+
+var localStorage = window.localStorage;
+localStorage.setItem("name","renee");
+var sessionStorage = window.sessionStorage;
+sessionStorage.setItem("age",19);
