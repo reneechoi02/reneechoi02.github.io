e8a1ba7764445f053028c492456c37dbeffcb3cd
reneechoi02 <reneechoi02@gmail.com>
2018-04-08 20:19:27 +0800

    add text to home page and update the nav title

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
-/**
- * Created by renee on 2017/4/10.
- */
+$(".btn-read-more").click(function(){
+    if($(".btn-read-more")[0].innerText=="阅读全文"){
+        $(".article-content")[0].style.height = "auto";
+        $(".btn-read-more")[0].innerText = "收起";
+    }else{
+        $(".article-content")[0].style.height = "20px";
+        $(".btn-read-more")[0].innerText = "阅读全文";
+    }
+
+})
+
+$(".nav-title").click(function(){
+    $(".nav-title a").each(function(){
+        $(this).removeClass("nav-active");
+    })
+    $(this).addClass("nav-active");
+})
 // if(window.localStorage){
 //     alert('This browser supports localStorage');
 // }else{
 //     alert('This browser does NOT support localStorage');
 // }
 
-var localArr = [];
-var localObj = {};
-var localStorage = window.localStorage;
-// localStorage.setItem("name","renee");
-var sessionStorage = window.sessionStorage;
-// sessionStorage.setItem("age",19);
-document.getElementById("localSave").onclick = function(){
-    var email =document.getElementById("email1").value;
-    var name = document.getElementById("name1").value;
-    localArr[localArr.length] = email;
-    localStorage.setItem("localArr",localArr);
-}
-
-var arr = localStorage.getItem("localArr");
-
-    for(var i=0;i<arr.length;i++) {
-        console.log(arr[i].email);
-        $("#localTable").append("<tr><td>" + arr[i].email + "</td><td>" + arr[i].name + "</td></tr>");
-    }
+// var localArr = [];
+// var localObj = {};
+// var localStorage = window.localStorage;
+// // localStorage.setItem("name","renee");
+// var sessionStorage = window.sessionStorage;
+// // sessionStorage.setItem("age",19);
+// document.getElementById("localSave").onclick = function(){
+//     var email =document.getElementById("email1").value;
+//     var name = document.getElementById("name1").value;
+//     localArr[localArr.length] = email;
+//     localStorage.setItem("localArr",localArr);
+// }
+//
+// var arr = localStorage.getItem("localArr");
+//
+//     for(var i=0;i<arr.length;i++) {
+//         console.log(arr[i].email);
+//         $("#localTable").append("<tr><td>" + arr[i].email + "</td><td>" + arr[i].name + "</td></tr>");
+//     }
 
 
