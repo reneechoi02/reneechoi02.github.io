a4c3a87f68d6fae3d30ef69b5ef367e25a21c857
reneechoi02 <reneechoi02@gmail.com>
2018-04-08 18:38:51 +0800

    add the first version of the home for person website

--- js/main.js
+++ js/main.js
@@ -7,7 +7,24 @@
 //     alert('This browser does NOT support localStorage');
 // }
 
+var localArr = [];
+var localObj = {};
 var localStorage = window.localStorage;
-localStorage.setItem("name","renee");
+// localStorage.setItem("name","renee");
 var sessionStorage = window.sessionStorage;
-sessionStorage.setItem("age",19);
+// sessionStorage.setItem("age",19);
+document.getElementById("localSave").onclick = function(){
+    var email =document.getElementById("email1").value;
+    var name = document.getElementById("name1").value;
+    localArr[localArr.length] = email;
+    localStorage.setItem("localArr",localArr);
+}
+
+var arr = localStorage.getItem("localArr");
+
+    for(var i=0;i<arr.length;i++) {
+        console.log(arr[i].email);
+        $("#localTable").append("<tr><td>" + arr[i].email + "</td><td>" + arr[i].name + "</td></tr>");
+    }
+
+
